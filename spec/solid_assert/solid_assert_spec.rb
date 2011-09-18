1616aa951c496c198fc2c5b9e5f67850f818988c
Jorge Manrubia <jorge.manrubia@gmail.com>
2011-09-19 00:48:50 +0200

    Spec for testing that assertions are disabled by default

--- spec/solid_assert/solid_assert_spec.rb
+++ spec/solid_assert/solid_assert_spec.rb
@@ -1,6 +1,14 @@
 require "spec_helper"
 
 describe SolidAssert::Assert do
+  it "should enable the empty assert method by default" do
+    assert false
+  end
+
+  it "should enable the empty invariant method by default" do
+    invariant{false}
+  end
+
   describe ".enable_assertions" do
     it "should make the Object class to include the Assertions module" do
       Object.should_receive(:include).with(SolidAssert::Assert)
